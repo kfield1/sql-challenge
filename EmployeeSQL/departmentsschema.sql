6b82c1309055dc0640c0dc3d4e9d8ac839840dce
kfield1 <kayt1303@yahoo.com>
2021-03-19 17:58:25 -0500

    added folder

---
+++ EmployeeSQL/departmentsschema.sql
@@ -0,0 +1,9 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL
+);
+
